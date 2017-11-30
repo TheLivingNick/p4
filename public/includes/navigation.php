c58e7f54eee01ff9ad52417dad8095bc1f4860bd
Nick Troy <nick.troy@gmail.com>
2017-11-29 23:25:17 -0500

    fixed some nav links

--- public/includes/navigation.php
+++ public/includes/navigation.php
@@ -1,10 +1,10 @@
     <div id="side_nav">
     <nav>
         <ul>
-            <li id="s-iamhome" class="top"><a href="/p4/public/">Home</a></li>
-            <li id="s-iamperson_profile"><a href="/p4/public/profile">Profile</a></li>
+            <li id="s-iamhome" class="top"><a href="/">Home</a></li>
+            <li id="s-iamperson_profile"><a href="/profile">Profile</a></li>
             <li id="s-iamfriends"><a href="friends.php">Friends</a></li>
-            <li id="s-iamlocation_profile"><a href="/p4/public/location">Locations</a></li>
+            <li id="s-iamlocation_profile"><a href="/location">Locations</a></li>
             <li id="s-iamnews"><a href="news.php">News</a></li>
             <li id="s-iamevents" class="bottom"><a href="events.php">Events</a></li>
         </ul>
